c4e9bb3910fd7bcd4268d22ac8fe13a8bcd9ca44
codeastrology-dev <codeastrology.dev2@gmail.com>
2021-10-27 11:07:41 +0600

    live reload not working on windows

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-function Hello(props){
+function Hello({library, data, gotcha}){
+  console.log(library);
   return (
     <div>
-      <h1>Hello World</h1>
-      <p>Welcome to this world</p>
+      <h1>Hello World {library}</h1>
+      <p>Welcome to this world {data}</p>
+      <p>Welcome to another line {gotcha}</p>
     </div>
   );
 }
 ReactDOM.render(
-  <Hello />,
+  <Hello
+  library="React App"
+  data="hello worlds"
+  gotcha = "sdadasd"
+  />,
   document.getElementById('root')
 );

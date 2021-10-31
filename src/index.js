21cd37f6b1c51cf4d958c7db6711e0b08bb915bf
codeastrology-dev <codeastrology.dev2@gmail.com>
2021-10-31 18:03:51 +0600

    working

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-function Hello({library, data, gotcha}){
-  console.log(library);
+
+function App(){
   return (
     <div>
-      <h1>Hello World {library}</h1>
-      <p>Welcome to this world {data}</p>
-      <p>Welcome to another line {gotcha}</p>
+      
     </div>
   );
 }
 ReactDOM.render(
-  <Hello
+  <App
   library="React App"
   data="hello worlds"
   gotcha = "sdadasd"
